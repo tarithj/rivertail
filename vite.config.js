8c375ef0c933faa0919e22a4d569fc0705c37753
Tarith Jayasooriya <tarithj@gmail.com>
2023-06-22 19:40:04 +0530

    Updated vite.config.js to get base from env var VITE_BASE

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig,loadEnv } from 'vite';
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
+
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: process.env["VITE_BASE"] ?? "/"
 });
+
